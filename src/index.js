3cfa0c66525c0d357cba7df50f1c3560f3ced4f1
sid978123 <siddharthaman978@gmail.com>
2025-11-29 19:22:56 +0530

    feat: update environment configuration and add cookie-parser and cors dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-dotenv.config({ path: ".env" });
+import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB();
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO  db connnection failed !!!", err);
+  });
