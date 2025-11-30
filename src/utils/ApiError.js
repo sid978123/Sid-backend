1af9937907e93a820ee34b5f4f6f9000b234a7ef
sid978123 <siddharthaman978@gmail.com>
2025-11-30 22:59:58 +0530

    fix: correct stack trace capture in ApiError and ensure proper initialization order

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -7,15 +7,15 @@ class ApiError extends Error {
   ) {
     super(message);
     this.statusCode = statusCode;
-    this.data = null;
     this.message = message;
-    this.success = false;
     this.errors = errors;
+    this.success = false;
+    this.data = null;
 
     if (stack) {
       this.stack = stack;
     } else {
-      Error: captureStackTrace(this, this.constructer);
+      Error.captureStackTrace(this, this.constructor);
     }
   }
 }
