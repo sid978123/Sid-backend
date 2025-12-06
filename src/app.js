25a3b7003ffc35bbacb85e5a8166a3e7cded2d52
sid978123 <siddharthaman978@gmail.com>
2025-12-07 02:29:26 +0530

    feat: implement user registration route and controller, and update cloudinary config

--- src/app.js
+++ src/app.js
@@ -37,4 +37,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 app.use(express.static("public"));
 
+//routes import ....
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
