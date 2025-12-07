1be15ba649c0fb70f98fa60c45e3b1f83b588fa1
sid978123 <siddharthaman978@gmail.com>
2025-12-07 15:31:29 +0530

    feat: refactor user registration route to include multer middleware for file uploads

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import ApiError from "./utils/ApiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { response } from "express";
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from the frontend
