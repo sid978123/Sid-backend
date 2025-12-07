590fe611344df0972d419aaf4edb85780596749c
sid978123 <siddharthaman978@gmail.com>
2025-12-07 16:00:26 +0530

    feat: add multer middleware for file uploads and update user registration logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from the frontend
@@ -27,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required..");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
