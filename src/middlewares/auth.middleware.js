fb9fe48d5269a9f01cb3ce444bba1068022fe8ab
sid978123 <siddharthaman978@gmail.com>
2025-12-15 00:21:05 +0530

    auth.middleware done

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,49 +3,51 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
+// export const verifyJWT = asyncHandler(async (req, res, next) => {
+//   try {
+//     const token =
+//       req.cookies?.accessToken ||
+//       req.header("Authorization")?.replace("Bearer ", "");
+
+//     if (!token) {
+//       throw new ApiError(401, "unauthorised request");
+//     }
+
+//     const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+//     const user = await User.findByIdAndUpdate(decodeToken?._id).select(
+//       "-password -refreshToken"
+//     );
+
+//     if (!user) {
+//       throw new ApiError(401, "Invalid Access Token");
+//     }
+
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     throw new ApiError(401, error?.message || "Invalid Access Token");
+//   }
+// });
+
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-
     if (!token) {
       throw new ApiError(401, "unauthorised request");
     }
-
     const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    const user = await User.findByIdAndUpdate(decodeToken?._id).select(
+    const user = await User.findByIdAndUpdate(decodeToken._id)?.select(
       "-password -refreshToken"
     );
-
     if (!user) {
-      throw new ApiError(401, "Invalid Access Token");
+      throw new ApiError(401, "invalid  token credentials");
     }
-
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401, error?.message || "Invalid Access Token");
+    throw new ApiError(403, error?.message || "invalid AccessToken ");
   }
 });
-
-// const verifyJWT = asyncHandler(async(req , res) =>{
-//   try {
-//     const token = await req.cookie?.accessToken || req.header("Authentication").replace("Bearer " , "")
-//     if(!token){
-//       throw new ApiError(401 , "token not exist")
-//     }
-//     const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-//     const user = await user.findByIdAndUpdate(decodeToken?._id).select(
-//       "-password -refreshToken"
-//     )
-
-//     if(!user){
-//       throw new ApiError()
-//     }
-
-//   } catch (error) {
-
-//   }
-// })
